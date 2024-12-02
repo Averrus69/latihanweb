0ec0e46eec841f6c5d5920252b16d2302869c616
Averrus69 <arryaverusai4@gmail.com>
2024-12-02 18:54:23 +0700

    updated

--- src/app/components/about/page.tsx
+++ src/app/components/about/page.tsx
@@ -2,17 +2,17 @@ export default function AboutMe() {
   return (
     <div className="justfiy-center">
       <p>
-        Hello! My name is Arry Averrus, and I'm an aspiring full-stack
+        Hello! My name is Arry Averrus, and I am an aspiring full-stack
         developer. Currently, I am diving into the world of coding at Purwadhika
-        Digital School, where I'm building a strong foundation in both front-end
-        and back-end development. As a newbie in the tech world, I'm passionate
-        about learning how things work behind the scenes of websites and
-        applications. I'm focused on mastering languages like HTML, CSS,
+        Digital School, where I am building a strong foundation in both
+        front-end and back-end development. As a newbie in the tech world, I am
+        passionate about learning how things work behind the scenes of websites
+        and applications. I am focused on mastering languages like HTML, CSS,
         JavaScript, and frameworks like React.js for front-end development. On
-        the back-end, I'm learning Node.js, Express, and working with databases
+        the back-end, I am learning Node.js, Express, and working with databases
         like MongoDB. My goal is to become a versatile developer, capable of
         working on both the client-side and server-side, and to build functional
-        and engaging applications. I'm excited to continue this learning
+        and engaging applications. I am excited to continue this learning
         journey, solve problems, and create projects that can make a difference.
         Thanks for taking the time to get to know me a little better!
       </p>
